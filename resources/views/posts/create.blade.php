eaf935adeced2521c1e0f92d4feb181f9e001ab5
Augusto <augusto.pinochet@alumnos.uv.cl>
2019-06-04 14:08:18 -0400

    Commit 2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,7 +6,9 @@
         <div class="row">
             <div class="cool-8 offset-4">
 
-
+                <div class="row">
+                    <div class="h1">Add new post</div>
+                </div>
                 <div class="form-group row">
                     <label for="caption" class="col-md-6 col-form-label">Post Caption</label>
 
@@ -16,22 +18,31 @@
                         caption="caption"
                         value="{{ old('caption') }}"
                         autocomplete="caption" autofocus>
-
-                            @error('caption')
-                            <span class="invalid-feedback" role="alert">
+                        @error('caption')
+                        <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
+                    </div>
+                    <div class="row">
+                        <label for="image" class="col-md-6 col-form-label">Post Image</label>
+
+                        <input type="file" class="form-control-file" id="image" name="image">
+
+                        @error('image')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class="row pt-4">
+                        <button class="btn btn-primary">Add New Post</button>
+                    </div>
+
+
                 </div>
-
-                <div class="row">
-                    <label for="image" class="col-md-6 col-form-label">Post Image</label>
-
-                    <input type="file", class="form-control-file" id="image" name="image">
-                </div>
-
             </div>
-        </div>
-    </form>
-</div>
-@endsection
+        </form>
+    </div>
+    @endsection
